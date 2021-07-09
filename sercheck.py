279426d996df8b85a53c8be3880f34466225cdf3
chiraag918 <39455997+chiraag918@users.noreply.github.com>
2021-07-09 08:41:55 +0530

    Update sercheck.py

--- sercheck.py
+++ sercheck.py
@@ -7,11 +7,11 @@ ser = serial.Serial("/dev/ttyS0",38400)
 while True:
     ser.write(b'1')
     
-    """rd=ser.read().decode('utf-8')
+    rd=ser.read().decode('utf-8')
     sleep(1)
     dl=ser.inWaiting()
     rd+=ser.read(dl)
-    print(rd)"""
+    print(rd)
     
     
     
