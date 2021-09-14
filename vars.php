135de10680dec8cf4282014f024cdc81d2dbdedf
chiraag918 <39455997+chiraag918@users.noreply.github.com>
2021-09-15 03:54:20 +0530

    Update vars.php

--- vars.php
+++ vars.php
@@ -4,7 +4,6 @@
 function move($dir){
 	switch ($dir) {	
 		case 'f': forward();	break;
-		case 'b': back();	break;
 		case 'r': right();	break;
 		case 'l': left();	break;
 		case 's': stop();	break;	
@@ -12,32 +11,17 @@ function move($dir){
 }
 
 function right(){
-    system("gpio -g mode 8 out");
-    system("gpio -g write 8 0");
-    system("gpio -g mode 7 out");
-    system("gpio -g write 7 1");
+    system("echo \"3\"> /dev/ttyS0 38400");
    
 }
 function left(){
-    system("gpio -g mode 8 out");
-    system("gpio -g write 8 1");
-    system("gpio -g mode 7 out");
-    system("gpio -g write 7 0");
+    system("echo \"2\"> /dev/ttyS0 38400");
 }
 function forward(){
-    system("gpio -g mode 8 out");
-    system("gpio -g write 8 1");
-    system("gpio -g mode 7 out");
-    system("gpio -g write 7 1");
-}
-function back(){
-	
+    system("echo \"1\"> /dev/ttyS0 38400");
 }
 function stop(){
-    system("gpio -g mode 8 out");
-    system("gpio -g write 8 0");
-    system("gpio -g mode 7 out");
-    system("gpio -g write 7 0");
+    system("echo \"1\"> /dev/ttyS0 38400");
 
 }
 
