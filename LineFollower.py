af96715a531f73219453ae52076601ff579e26d0
chiraag918 <39455997+chiraag918@users.noreply.github.com>
2021-09-15 03:46:25 +0530

    Update LineFollower.py

--- LineFollower.py
+++ LineFollower.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 from gpiozero import LED
+from serial import Serial
 
-motor1 = LED(8)
-motor2 = LED(7)
+ser = serial.Serial("/dev/ttyS0",38400)
 
 cap = cv2.VideoCapture(0)
 cap.set(3,160)
@@ -28,16 +28,13 @@ while True:
             print("CX:"+str(cx)+" CY:"+str(cy))
             if cx>=320:
                 print("Turn Right")
-                motor1.off()
-                motor2.on()
+                ser.write(b'3')
             if cx<320 and cx>300:
                 print("On track")
-                motor1.on()
-                motor2.on()
+                ser.write(b'1')
             if cx<=300:
                 print("Turn Left")
-                motor1.on()
-                motor2.off()
+                ser.write(b'2')
             cv2.circle(frame,(cx,cy),5,(255,255,255),-1)
     cv2.drawContours(frame,c,-1,(0,255,0),1)
     cv2.imshow("Mask",mask)
